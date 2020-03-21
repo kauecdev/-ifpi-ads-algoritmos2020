074917c5309f579f365151c1863c7eca69d83762
Kauê Cavalcante <61801350+kauecdev@users.noreply.github.com>
2020-03-21 11:37:36 -0300

    Add files via upload

--- script imposto/imposto_e_roubo.py
+++ script imposto/imposto_e_roubo.py
@@ -1,20 +1,69 @@
 def main():
-    renda = float(input("Digite sua renda R$: "))
+    print("Cálculo do Imposto de Renda com a tabela sem correção e com tabela corrigida.")
 
-    taxa = calcular_imposto(renda)
-    imposto = renda * (taxa/100)
+    renda = float(input("Insira sua renda mensal: R$ "))
 
-    print(f"Você será tributado em {taxa}% de taxa.")
-    print(f"Logo, pagará R${imposto} em imposto.")
+    ir_antigo = calcular_ir_sem_reajuste(renda)
+    ir_reajustado = calcular_ir_com_reajuste(renda)
 
-def calcular_imposto(renda):
-    if renda <= 1900:
+    print(f"De acordo com a tabela sem reajuste, quem recebe R$ {renda}, deverá pagar R$ {ir_antigo:.2f} de imposto de renda.\n"
+    f"Já de acordo com a tabela reajustada, quem recebe R$ {renda}, deverá pagar R$ {ir_reajustado:.2f} de imposto de renda.")
+
+
+def calcular_ir_sem_reajuste(r):
+    taxa_1 = 1903.98
+    taxa_2 = 2826.65
+    taxa_3 = 3751.05
+    taxa_4 = 4664.68
+
+    if r <= taxa_1: 
         return 0
-    elif renda <= 3000:
-        return 14
-    elif renda <= 8000:
-        return 22
+    elif r <= taxa_2:
+        r -= taxa_1
+        return (7.5 / 100) * r
+    elif r <= taxa_3:
+        r1 = (7.5 / 100) * (taxa_2 - taxa_1)
+        r -= taxa_2
+        return ((15 / 100) * r) + r1
+    elif r <= taxa_4:
+        r1 = (7.5 / 100) * (taxa_2 - taxa_1)
+        r2 = (15 / 100) * (taxa_3 - taxa_2)
+        r -= taxa_3
+        return ((22.5 / 100) * r) + r1 + r2
     else:
-        return 27
+        r1 = (7.5 / 100) * (taxa_2 - taxa_1)
+        r2 = (15 / 100) * (taxa_3 - taxa_2)
+        r3 = (22.5 / 100) * (taxa_4 - taxa_3)
+        r -= taxa_4
+        return ((27.5 / 100) * r) + r1 + r2 + r3
+
+
+def calcular_ir_com_reajuste(r):
+    taxa_1 = 3881.65
+    taxa_2 = 5714.11
+    taxa_3 = 7654.67
+    taxa_4 = 9564.42
+
+    if r <= taxa_1: 
+        return 0
+    elif r <= taxa_2:
+        r -= taxa_1
+        return (7.5 / 100) * r
+    elif r <= taxa_3:
+        r1 = (7.5 / 100) * (taxa_2 - taxa_1)
+        r -= taxa_2
+        return ((15 / 100) * r) + r1
+    elif r <= taxa_4:
+        r1 = (7.5 / 100) * (taxa_2 - taxa_1)
+        r2 = (15 / 100) * (taxa_3 - taxa_2)
+        r -= taxa_3
+        return ((22.5 / 100) * r) + r1 + r2
+    else:
+        r1 = (7.5 / 100) * (taxa_2 - taxa_1)
+        r2 = (15 / 100) * (taxa_3 - taxa_2)
+        r3 = (22.5 / 100) * (taxa_4 - taxa_3)
+        r -= taxa_4
+        return ((27.5 / 100) * r) + r1 + r2 + r3
+
 
 main()
